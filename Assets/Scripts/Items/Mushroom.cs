433df9b456c411e3fb41c12bdf3401f69d1716c2
Thomas <thomas.picard@isen.yncrea.fr>
2022-04-11 14:51:16 +0200

    Mushroom + Bomb

--- Assets/Scripts/Items/Mushroom.cs
+++ Assets/Scripts/Items/Mushroom.cs
@@ -11,6 +11,10 @@ public class Mushroom : Item
 
     protected override void OnCollisionEnter(Collision collision)
     {
-        
+        Player player = collision.gameObject.GetComponent<Player>();
+        if (player != null)
+        {
+            Destroy(this.gameObject);
+        }
     }
 }
