a73925a39724b1a97f72b0c78a2c5544bd983073
Marion0401 <71750635+Marion0401@users.noreply.github.com>
2022-04-20 14:13:14 +0200

    scene de fin ok

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -9,7 +9,16 @@ public class UIManager : MonoBehaviour
     public Text Score1;
     public Text Score2;
     public Text Timer;
+    public Text Score1End;
+    public Text Score2End;
+    public GameObject image;
+    public GameObject Camera;
+    public GameObject CanvasEndGame;
+    public AudioSource MusicEnd;
+    public List<AudioSource> AudiosGame = new List<AudioSource>();
+    
     private static UIManager instance;
+    private Vector3 camPosition;
 
     private void Awake()
     {
@@ -17,6 +26,7 @@ public class UIManager : MonoBehaviour
     }
     void Start()
     {
+        camPosition = Camera.transform.position;
         Score1.text = Field.Team2.ConcededGoals.ToString();
         Score2.text = Field.Team1.ConcededGoals.ToString();
     }
@@ -32,5 +42,22 @@ public class UIManager : MonoBehaviour
     {
         instance.Score1.text = Field.Team2.ConcededGoals.ToString();
         instance.Score2.text = Field.Team1.ConcededGoals.ToString();
+    }
+
+    public static void TimeOut()
+    {
+        instance.Camera.transform.position = instance.camPosition;
+        instance.CanvasEndGame.SetActive(true);
+        instance.image.gameObject.SetActive(false);
+        instance.Timer.gameObject.SetActive(false);
+        instance.Score1.gameObject.SetActive(false);
+        instance.Score2.gameObject.SetActive(false);
+
+        foreach (AudioSource audio in instance.AudiosGame)
+        {
+            audio.Stop();
+        }
+        instance.Score1End.text = Field.Team2.ConcededGoals.ToString();
+        instance.Score2End.text = Field.Team1.ConcededGoals.ToString();
     }
 }
