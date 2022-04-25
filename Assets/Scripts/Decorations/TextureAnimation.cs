0af03e80627884ed96fc9e59489b90e564a638a6
ComeSerlooten <71868443+ComeSerlooten@users.noreply.github.com>
2022-04-25 15:09:26 +0200

    Lumières, audience, animation décor

--- Assets/Scripts/Decorations/TextureAnimation.cs
+++ Assets/Scripts/Decorations/TextureAnimation.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class TextureAnimation : MonoBehaviour
 {
     public float offset = 0;
-    public int property = 1;
+    public int property = 5;
+    [Range(0,2f)] public float speed = 0.5f;
     Material mat;
     // Start is called before the first frame update
     void Start()
@@ -16,7 +17,7 @@ public class TextureAnimation : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        offset += Random.Range(-8*Time.deltaTime, 10 * Time.deltaTime);
+        offset += speed * Random.Range(-8*Time.deltaTime, 10 * Time.deltaTime);
         //offset = Random.Range(0f, 1);
         mat.SetTextureOffset(property, new Vector2(offset, 0));
             //SetTextureOffset("_Emmission", new Vector2(offset,0));
