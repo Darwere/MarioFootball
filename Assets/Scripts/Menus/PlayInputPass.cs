c694affbbb5005fd922b172cb1470ece84a0a7a7
ComeSerlooten <71868443+ComeSerlooten@users.noreply.github.com>
2022-04-26 11:39:03 +0200

    Préparations Passage PlayerInput

---
+++ Assets/Scripts/Menus/PlayInputPass.cs
@@ -0,0 +1,103 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Users;
+using UnityEngine.SceneManagement;
+
+public class PlayInputPass : MonoBehaviour
+{
+    PlayerInputManager PInputManager;
+    List<PlayerInput> menuPInput = new List<PlayerInput>();
+    bool onMenu = true;
+
+    GameObject Team1 = null;
+    PlayerInput T1PInput;
+    bool T1got = false;
+    GameObject Team2 = null;
+    PlayerInput T2PInput;
+    bool T2got = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PInputManager = FindObjectOfType<PlayerInputManager>();
+        PInputManager.onPlayerJoined += PlayerJoin;
+        SceneManager.sceneLoaded += GetGamePlayerInput;
+        
+    }
+
+    public void GetGamePlayerInput(Scene scene, LoadSceneMode mode)
+    {
+        if(scene.name == "Game")
+        {
+            Team1 = Field.Team1.gameObject;
+            Team2 = Field.Team2.gameObject;
+            onMenu = false;
+        }
+        else
+        {
+            Team1 = null;
+            Team2 = null;
+            T1got = false;
+            T2got = false;
+            onMenu = true;
+        }
+    }
+
+    private void PlayerJoin(PlayerInput playerInput)
+    {
+        if(onMenu)
+        {
+            menuPInput.Add(playerInput);
+        }
+    }
+
+
+    private void OnPlayerLeft(PlayerInput playerInput)
+    {
+        if (onMenu)
+        {
+            menuPInput.Remove(playerInput);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Team1 != null && Team2 !=null)
+        {
+            if(Team1.GetComponent<PlayerInput>())
+            {
+                T1PInput = Team1.GetComponent<PlayerInput>();
+                T1got = true;
+            }
+            if (Team2.GetComponent<PlayerInput>())
+            {
+                T2PInput = Team2.GetComponent<PlayerInput>();
+                T2got = true;
+            }
+
+            if(T1got && T2got && menuPInput.Count == 2)
+            {
+                InputUser p1 = T1PInput.user;
+                p1.UnpairDevices();
+                foreach (InputDevice device in menuPInput[0].devices)
+                {
+                    InputUser.PerformPairingWithDevice(device, p1);
+                }
+
+                InputUser p2 = T2PInput.user;
+                p2.UnpairDevices();
+                foreach (InputDevice device in menuPInput[1].devices)
+                {
+                    InputUser.PerformPairingWithDevice(device, p2);
+                }
+
+                Destroy(this.gameObject);
+
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Menus/PlayerInputInfoRead.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInputInfoRead : MonoBehaviour
+{
+    [SerializeField] PlayerInput pinput1;
+    [SerializeField] List<string> dev = new List<string>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        dev = new List<string>();
+        foreach (InputDevice ID in pinput1.devices)
+        {
+            dev.Add(ID.name);
+        }
+
+    }
+}
